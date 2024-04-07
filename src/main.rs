8f17ee37f61e4d208499dc43214383db24d203ab
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2024-04-07 15:59:41 +0200

    Add initial functionality for creating tar files

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,178 @@ fn main() -> Result<(), Box<dyn Error>> {
             let mut f = File::create(file.file_name)?;
             f.write(file.body.as_bytes())?;
         }
+    } else if args.create {
+        // Create archive
+        if let Some(tar_filename) = args.file {
+            let mut tar_file = File::create(tar_filename)?;
+            // let mut blocks: Vec<[u8; 512]> = Vec::new();
+            let mut blocks: Vec<u8> = Vec::new();
+
+            for file in args.input_files {
+                let mut f = File::open(&file)?;
+                let metadata = f.metadata()?;
+                let file_mode = metadata.permissions().mode();
+                let file_size_met = metadata.len();
+                let mtime = metadata
+                    .modified()?
+                    .duration_since(SystemTime::UNIX_EPOCH)?
+                    .as_secs();
+
+                let uid = metadata.uid();
+                let gid = metadata.gid();
+                let file_type = metadata.file_type();
+                let type_flag = match file_type {
+                    _ if file_type.is_file() => Ok('0'),
+                    _ if file_type.is_dir() => Ok('5'),
+                    _ if file_type.is_symlink() => Ok('2'),
+                    _ if file_type.is_block_device() => Ok('4'),
+                    _ if file_type.is_char_device() => Ok('3'),
+                    _ if file_type.is_fifo() => Ok('6'),
+                    _ => Err("Type flag is not supported!"),
+                }?;
+
+                let linkname = if file_type.is_symlink() {
+                    fs::read_link(&file)?.to_string_lossy().into_owned()
+                } else {
+                    String::new()
+                };
+
+                let mut username = String::new();
+                let mut group_name = String::new();
+
+                unsafe {
+                    let user = getpwuid(uid);
+                    if !user.is_null() {
+                        let name = CStr::from_ptr((*user).pw_name);
+                        let usr = name.to_str().map_err(|_| "Couldn't get user name!")?;
+                        username = usr.to_owned();
+                    }
+
+                    let group = getgrgid(gid);
+                    if !group.is_null() {
+                        let name = CStr::from_ptr((*group).gr_name);
+                        let grp = name.to_str().map_err(|_| "Couldn't get group name!")?;
+                        group_name = grp.to_owned();
+                    }
+                }
+
+                // let dev = metadata.dev();
+                // let devmajor = (dev >> 8) as u8;
+                // let devminor = (dev & 0xFF) as u8;
+
+                let (prefix, name) = if file.len() > 100 {
+                    let (prefix, name) = file.split_at(file.len() - 100);
+                    (prefix, name)
+                } else {
+                    ("", file.as_str())
+                };
+        
+                let mut header = [0u8; 512];
+
+                let mut cursor = 0;
+
+                write!(&mut header[cursor..cursor+100], "{:<100}", name)?; // Name
+                cursor += 100;
+                write!(&mut header[cursor..cursor+8], "{:07o}", file_mode & 0o0777)?; // Mode
+                cursor += 8;
+                write!(&mut header[cursor..cursor+8], "{:07o}", uid)?; // UID
+                cursor += 8;
+                write!(&mut header[cursor..cursor+8], "{:07o}", gid)?; // GID
+                cursor += 8;
+                write!(&mut header[cursor..cursor+12], "{:011o}", file_size_met)?; // Size
+                cursor += 12;
+                write!(&mut header[cursor..cursor+12], "{:011o}", mtime)?; // MTime
+                cursor += 12;
+                write!(&mut header[cursor..cursor+8], "{:8}", " ")?; // Checksum
+                cursor += 8;
+                write!(&mut header[cursor..cursor+1], "{}", type_flag)?; // Typeflag
+                cursor += 1;
+                write!(&mut header[cursor..cursor+100], "{:<100}", linkname)?; // Linkname
+                cursor += 100;
+                write!(&mut header[cursor..cursor+6], "ustar ")?; // Magic
+                cursor += 6;
+                write!(&mut header[cursor..cursor+2], "{:<2}", " \0")?; // Version
+                cursor += 2;
+                write!(&mut header[cursor..cursor+32], "{:<32}", username)?; // Uname
+                cursor += 32;
+                write!(&mut header[cursor..cursor+32], "{:<32}", group_name)?; // Gname
+                cursor += 32;
+                // write!(&mut header[cursor..cursor+8], "{:07o}", devmajor)?; // Devmajor
+                write!(&mut header[cursor..cursor+8], "{:07}", "\0")?; // Devmajor <- Apparently in the file created by the tar command these are 0 bytes
+                cursor += 8;
+                //write!(&mut header[cursor..cursor+8], "{:07o}", devminor)?; // Devminor
+                write!(&mut header[cursor..cursor+8], "{:07}", "\0")?; // Devminor <- Apparently in the file created by the tar command these are 0 bytes
+                cursor += 8;
+                write!(&mut header[cursor..cursor+155], "{:<155}", prefix)?; // Prefix
+
+                for i in 0..512 {
+                    if header[i] == 32 && i != 155 && i != 262 && i != 263 {
+                        header[i] = 0;
+                    }
+                }
+
+                let checksum: u32 = header.iter().map(|&b| u32::from(b)).sum();
+
+                write!(&mut header[148..156], "{:<06o}\0", checksum)?; // Checksum
+
+                blocks.extend(header);
+
+                let mut buffer = Vec::new();
+                f.read_to_end(&mut buffer)?;
+
+                // TODO: Make this work with files that are not a multiple of 512 bytes, but bigger than 512 bytes
+                if buffer.len() < 512 {
+                    let padding = vec![0; 512 - buffer.len()];
+                    buffer.extend(padding);
+                } else if buffer.len() > 512 {
+                    let padding = vec![0; 512 - (buffer.len() % 512)];
+                    buffer.extend(padding);
+                }
+
+                for i in (0..buffer.len()).step_by(512) {
+                    let mut block = [0u8; 512];
+                    block.copy_from_slice(&buffer[i..i+512]);
+                    blocks.extend(block);
+                }
+
+                /* println!("File size: {:?}", tar_file.metadata()?.len());
+
+                println!("{:?}", header);
+
+                // FIX: For some reason when it is writing the header for the second file it only gets to the point where the filename ends
+                if let Ok(bytes_written) = tar_file.write(&header) {
+                    if bytes_written != 512 {
+                        return Err("Couldn't write header to tar file!".into());
+                    }
+
+                    println!("{:?}", buffer);
+                    
+                    if let Ok(bytes_written) = tar_file.write(&buffer) {
+                        if bytes_written != buffer.len() {
+                            return Err("Couldn't write file to tar file!".into());
+                        }
+                    }
+                }
+
+                println!("File size: {:?}", tar_file.metadata()?.len()); */
+            }
+
+            println!("Buffer length pre write: {:?}", blocks.len());
+
+            let padding_blocks = if blocks.len() < 20 * 512 {
+                20 - (blocks.len() / 512)
+            } else {
+                2
+            };
+
+            for _ in 0..padding_blocks {
+                blocks.extend([0u8; 512]);
+            }
+
+            println!("Buffer length post write: {:?}", blocks.len());
+
+            tar_file.write_all(&blocks)?;
+        }
     }
 
     Ok(())
--- src/utils/arg_parser.rs
+++ src/utils/arg_parser.rs
@@ -6,6 +6,8 @@ pub struct Arguments {
     pub list: bool,
     pub file: Option<String>,
     pub extract: bool,
+    pub create: bool,
+    pub input_files: Vec<String>,
 }
 
 pub fn setup_parser() -> Result<Arguments, Box<dyn Error>> {
@@ -31,6 +33,19 @@ pub fn setup_parser() -> Result<Arguments, Box<dyn Error>> {
                 .num_args(0)
                 .help("Extracts the contents of an archive")
                 .conflicts_with_all(&["list", "create"]),
+            Arg::new("create")
+                .short('c')
+                .long("create")
+                .num_args(0)
+                .help("Creates a new archive")
+                .conflicts_with_all(&["extract", "list"])
+                .requires("file"),
+            Arg::new("input_files")
+                .index(1)
+                .num_args(1..)
+                .required(false)
+                .help("Input files to be added to the archive")
+                .conflicts_with_all(&["list", "extract"]),
         ])
         .get_matches();
 
@@ -45,10 +60,24 @@ pub fn setup_parser() -> Result<Arguments, Box<dyn Error>> {
         .get_one::<bool>("extract")
         .map(|extract| *extract)
         .unwrap_or_else(|| false);
+    let create = matches
+        .get_one::<bool>("create")
+        .map(|create| *create)
+        .unwrap_or_else(|| false);
+    let input_files = matches
+        .get_many::<String>("input_files")
+        .map(|values| {
+            values
+                .map(|value| value.to_string())
+                .collect::<Vec<String>>()
+        })
+        .unwrap_or_default();
 
     Ok(Arguments {
         list,
         file,
         extract,
+        create,
+        input_files,
     })
 }

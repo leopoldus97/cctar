be483baa4ebff044331254bc00d2e0c45e9618a2
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2024-04-14 18:23:49 +0200

    Move functions from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,26 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, Read},
+};
+
+use tar::models::TarArchive;
+
 pub mod tar;
 pub mod utils;
+
+pub fn load_archive(file_name: Option<String>) -> Result<TarArchive, Box<dyn Error>> {
+    let from_stdin = file_name.is_none();
+    let buffer = load_file(file_name)?;
+
+    let archive = TarArchive::from_reader(buffer, from_stdin)?;
+    Ok(archive)
+}
+
+fn load_file(file_path: Option<String>) -> Result<Box<dyn Read>, Box<dyn Error>> {
+    if let Some(file) = file_path {
+        Ok(Box::new(File::open(file)?))
+    } else {
+        Ok(Box::new(io::stdin()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::{
-    error::Error,
-    fs::File,
-    io::{self, Read, Write},
-};
+use std::{error::Error, fs::File, io::Write};
 
-use cctar::{tar::models::TarArchive, utils::arg_parser::setup_parser};
+use cctar::{load_archive, tar::models::TarArchive, utils::arg_parser::setup_parser};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = setup_parser()?;
@@ -31,20 +27,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-fn load_file(file_path: Option<String>) -> Result<Box<dyn Read>, Box<dyn Error>> {
-    if let Some(file) = file_path {
-        Ok(Box::new(File::open(file)?))
-    } else {
-        Ok(Box::new(io::stdin()))
-    }
-}
-
-fn load_archive(file_name: Option<String>) -> Result<TarArchive, Box<dyn Error>> {
-    let from_stdin = file_name.is_none();
-    let buffer = load_file(file_name)?;
-
-    let archive = TarArchive::from_reader(buffer, from_stdin)?;
-    Ok(archive)
 }
